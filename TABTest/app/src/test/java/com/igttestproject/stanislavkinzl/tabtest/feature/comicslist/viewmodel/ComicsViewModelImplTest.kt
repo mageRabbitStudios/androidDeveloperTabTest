cc41d1bd4a4ecdd0f999ac046e585952272291c5
lopasik <kinzlstanislav@gymnachod.cz>
2018-11-06 18:14:49 +0000

    playing around while making tests

---
+++ TABTest/app/src/test/java/com/igttestproject/stanislavkinzl/tabtest/feature/comicslist/viewmodel/ComicsViewModelImplTest.kt
@@ -0,0 +1,15 @@
+package com.igttestproject.stanislavkinzl.tabtest.feature.comicslist.viewmodel
+
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class ComicsViewModelImplTest {
+
+    @Before
+    fun setUp() {
+
+    }
+}
