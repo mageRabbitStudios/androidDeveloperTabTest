443c968f279391fa94019cef9ea9cac800f7e349
lopasik <kinzlstanislav@gymnachod.cz>
2018-11-08 12:48:50 +0000

    Updating to gradle 3.2.1
    In order to update from 3.0.0, ned mess dependencies etc. I spent many hours figuring out how to do it. At the end I was forced to create a new project with the same classes, that used the new gradle (new gradle is required for functionality of Robolectric). So this should be behind me and I should now fully focus on locak android-framework tests. We'll see how I'll try to make an widget test

--- TABTest/app/src/main/java/com/igttestproject/stanislavkinzl/tabtest/app/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.igttestproject.stanislavkinzl.tabtest.app
-
-const val API_KEY = "d69b9581419ed36b6ef230f1f4e41fa4"
-const val PRIVATE_KEY = "0b7ac65c6413040c06b6f0a8ac04251e4a3cf16e"
-
-const val NUMBER_OF_COMICS_PER_ROW_PORTRAIT = 3
-const val NUMBER_OF_COMICS_PER_ROW_LANDSCAPE = 5
