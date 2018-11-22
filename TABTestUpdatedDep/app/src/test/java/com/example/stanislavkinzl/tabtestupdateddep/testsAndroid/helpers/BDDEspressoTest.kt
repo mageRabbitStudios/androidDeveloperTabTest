23aca3490a9e0f1e63c126c88111a4cf1a1a812b
lopasik <kinzlstanislav@gymnachod.cz>
2018-11-22 12:03:35 +0000

    making almost all test pass

--- TABTestUpdatedDep/app/src/test/java/com/example/stanislavkinzl/tabtestupdateddep/testsAndroid/helpers/BDDEspressoTest.kt
+++ TABTestUpdatedDep/app/src/test/java/com/example/stanislavkinzl/tabtestupdateddep/testsAndroid/helpers/BDDEspressoTest.kt
@@ -5,6 +5,7 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import androidx.test.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
 import com.example.stanislavkinzl.tabtestupdateddep.testsAndroid.TestApp
 import org.junit.Rule
 
@@ -32,9 +33,11 @@ abstract class ActivityBDDEspressoTest <T : Activity> : BDDEspressoTest() {
     /*//TODO: Is this lax solution ? Maybe it could be done better?
     lateinit var activityRule: ActivityTestRule<T>*/
 
-    abstract val clazz: Class<T>
+//    abstract val clazz: Class<T>
 
-    @JvmField
+
+
+   /* @JvmField
     @Rule
     var activityRule: ClearedPrefsActivityTestRuxle<out T> = ClearedPrefsActivityTestRule(clazz,
         false,
@@ -49,7 +52,10 @@ abstract class ActivityBDDEspressoTest <T : Activity> : BDDEspressoTest() {
     }
 
     protected val targetContext: Context
-        get() = InstrumentationRegistry.getTargetContext()
+        get() = InstrumentationRegistry.getTargetContext()*/
+
+    lateinit var activityRule: ActivityTestRule<T>
+    val launchFirst = true
 
 
     fun Given(step: () -> Unit, launchFirst: Boolean = true) {
